691173ce7ba446c576225fc927b321ef0e6eb58a
GAL_son <pancerek1@gmail.com>
2023-11-20 22:52:36 +0100

    libs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 import Button from './components/Button';
+import MovieCard from './components/MovieCard'
+import Navigation from './components/Navigation';
+import ProfileBubble from './components/ProfileBubble';
 
 function App() {
   return (
     <div className="App">
-      <Button className="btn btn-primary" title="button" onClick={()=>{alert("LMAO")}}/>
+      <Button className="btn-primary" title="button" onClick={()=>{alert("LMAO")}}/>
+      <MovieCard/>
+      <ProfileBubble/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navigation/>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
