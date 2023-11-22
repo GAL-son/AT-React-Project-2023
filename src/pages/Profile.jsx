00936faf8aaeb54a8860abd7da2dc865ce4ff0cd
GAL_son <pancerek1@gmail.com>
2023-11-22 21:50:30 +0100

    Preview

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,11 @@
 import MovieListPreview from "../components/MovieListPreview";
-import ProfileLink from "../components/ProfileLink";
 import ProfileList from "../components/ProfileList";
 import ProfilePicture from "../components/ProfilePicture";
 
 const testFriends = ['Antony',"Anna123","TestNull","Robert"]
 
 const testParams = {
-    username: 'Test123',
+    username: 'Galson',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam a earum ipsa eligendi molestiae id dicta quidem, dolor possimus doloremque, vel totam iusto distinctio numquam unde enim sapiente tempore maxime.'
 }
 
@@ -20,7 +19,7 @@ const Profile = (params) => {
                     <p>{testParams.desc}</p>
                     <div>
                     <h3>Friends</h3>
-                        <ProfileList profileList={testFriends}/>
+                        <ProfileList profiles={testFriends}/>
                     </div>
                 </div>
                
